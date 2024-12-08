0b32f054295cedd8fe15f2a72b2bb659d73b9041
Joseph.ma <josephma.kw@gmail.com>
2024-12-07 23:09:13 -0700

    base files

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+// index.php
+include 'includes/header.php';
+?>
+
+<div class="login-container">
+    <h2>Login</h2>
+    <form action="login.php" method="POST">
+        <input type="text" name="username" placeholder="Username" required>
+        <input type="password" name="password" placeholder="Password" required>
+        <button type="submit">Login</button>
+    </form>
+</div>
+
+<?php
+include 'includes/footer.php';
+?>
